92c9e0c0544086855836d8c76d860c5325c40525
hoangtien1612 <108447557+hoangtien1612@users.noreply.github.com>
2022-07-07 10:11:24 +0700

    Update fmcg-analysis.sql

--- fmcg-analysis.sql
+++ fmcg-analysis.sql
@@ -1,10 +1,10 @@
-SQL Project: Analyze FMCG's Sales Department
+-- SQL Project: Analyze FMCG's Sales Department
 
-In this project, we will go through unreal FMCG's firm - A company distributes food, beer and beverage to mom-and-pop stores in Vietnam. In term as a member of Sales Analyst team, we'll answer some questions about current business situation of this firm.
+-- In this project, we will go through unreal FMCG's firm - A company distributes food, beer and beverage to mom-and-pop stores in Vietnam. In term as a member of Sales Analyst team, we'll answer some questions about current business situation of this firm.
 
-We've asked for solving these tasks below:
+-- We've asked for solving these tasks below:
 
-Task 1: Quick view about total retailers, active retailers (rate), orders, Total GMV, promotion(GMV), refund stock(GMV).
+-- Task 1: Quick view about total retailers, active retailers (rate), orders, Total GMV, promotion(GMV), refund stock(GMV).
 
 
 select
